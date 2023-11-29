b3ffde0ab97c161f87b5cf391360e4cd06a94ea4
Willy Arrojas <willyarrojas@gmail.com>
2023-11-29 16:19:53 +0100

    add topics until 257

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const { dbConnection } = require('./database/config')
 const cors = require('cors');
 const { TypeAPI } = require('./enum/shared.enum');
+const path = require('path');
 
 // Crear el servidor de express
 const app = express();
@@ -19,13 +20,17 @@ app.use(express.json());
 // Start BBDD
 dbConnection()
 
-// Rutas
+// Routes
 app.use(`/${TypeAPI.API}/${TypeAPI.USERS}`, require('./routes/users'));
 app.use(`/${TypeAPI.API}/${TypeAPI.HOSPITALS}`, require('./routes/hospitals'));
 app.use(`/${TypeAPI.API}/${TypeAPI.DOCTORS}`, require('./routes/doctors'));
 app.use(`/${TypeAPI.API}/${TypeAPI.ALL}`, require('./routes/search'));
 app.use(`/${TypeAPI.API}/${TypeAPI.UPLOADS}`, require('./routes/uploads'));
 app.use(`/${TypeAPI.API}/${TypeAPI.LOGIN}`, require('./routes/auth'));
+// latest
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'public/index.html'));
+});
 
 app.listen(process.env.PORT, () => {
     console.log('Server running in the port: ' + process.env.PORT);
